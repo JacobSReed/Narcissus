d41ed3b2fe6226c8eb25cd188958450ddaa38964
Bagglesss <129259015+JacobSReed@users.noreply.github.com>
2023-12-15 16:41:08 +1100

    Redirects on main app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    const url = window.location.pathname.toLowerCase();
+    // Check if the current URL is /QR
+    if (url.includes('/qr')) {
+      // Redirect to the main page
+      window.location.href = '/';
+    }
+    if (url.includes('/rick')) {
+      // Redirect to the main page
+      window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+    }
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
